e6825c7a44761f384d24276424d6839429d6978d
origin <https://github.com/shunyibai/face.git>
2018-03-14 22:41:34 +0800

    尝试调试，灰质

--- app/src/main/java/com/example/shunyibai/opencvdemo/MainActivity.java
+++ app/src/main/java/com/example/shunyibai/opencvdemo/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.shunyibai.opencvdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.OpenCVLoader;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button buClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buClick = (Button) findViewById(R.id.bu_click_mainAty);
+        buClick.setOnClickListener(this);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, getApplicationContext(), mLoaderCallback);
+    }
+    //OpenCV库加载并初始化成功后的回调函数
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+
+        @Override
+        public void onManagerConnected(int status) {
+            // TODO Auto-generated method stub
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:
+                    Log.i("tag", "成功加载");
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    Log.i("tag", "加载失败");
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bu_click_mainAty:
+                break;
+        }
     }
 }
